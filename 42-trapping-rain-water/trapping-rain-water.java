83ecc05cf4466b7212c070df31404e4ec7104370
VIVEK YADAV <vivekyadavlucknow3123@gmail.com>
2026-08-15 17:44:45 +0530

    Time: 1 ms (58.48%) | Memory: 47.6 MB (68.93%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        
+        int left = 0;
+        int right = height.length - 1;
+        
+        int leftMax = 0;
+        int rightMax = 0;
+        int totalWater = 0;
+        
+        while (left < right) {
+            // We are bottlenecked by the smaller side
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left]; // Update max on the left
+                } else {
+                    totalWater += leftMax - height[left]; // Add trapped water
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right]; // Update max on the right
+                } else {
+                    totalWater += rightMax - height[right]; // Add trapped water
+                }
+                right--;
+            }
+        }
+        
+        return totalWater;
+    }
+}
